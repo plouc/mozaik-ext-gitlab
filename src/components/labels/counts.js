893fabdd63d97a3c1327ea6c18da13e7f6e78c0d
Raphaël Benitte <raphael.benitte@fullsix.com>
2017-08-03 15:06:27 +0900

    feat(labels): improve label charts api data validation

--- src/components/labels/counts.js
+++ src/components/labels/counts.js
@@ -1,7 +1,7 @@
 export const countByLabels = {
     open_issues_count: 'open issues',
     closed_issues_count: 'closed issues',
-    open_merge_requests_count: 'open merge requests',
+    open_merge_requests_count: 'opened merge requests',
 }
 
 export const countTypes = Object.keys(countByLabels)
--- src/components/labels/propTypes.js
+++ src/components/labels/propTypes.js
@@ -4,9 +4,24 @@ import { countTypes } from './counts'
 export const labelsPropTypes = {
     project: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
     countBy: PropTypes.oneOf(countTypes).isRequired,
-    apiData: PropTypes.arrayOf(PropTypes.any).isRequired,
+    apiData: PropTypes.shape({
+        project: PropTypes.shape({
+            name: PropTypes.string.isRequired,
+            web_url: PropTypes.string.isRequired,
+        }).isRequired,
+        labels: PropTypes.arrayOf(
+            PropTypes.shape({
+                id: PropTypes.number.isRequired,
+                name: PropTypes.string.isRequired,
+                color: PropTypes.string.isRequired,
+                open_issues_count: PropTypes.number.isRequired,
+                closed_issues_count: PropTypes.number.isRequired,
+                open_merge_requests_count: PropTypes.number.isRequired,
+            })
+        ).isRequired,
+    }),
     apiError: PropTypes.object,
-    animate: PropTypes.func.isRequired,
+    animate: PropTypes.bool.isRequired,
 }
 
 export const labelsDefaultProps = {
