2ed0cca8390a6aea63a8f3932b37e3637e6d5044
Raphaël Benitte <raphael.benitte@fullsix.com>
2017-08-03 15:33:40 +0900

    feat(labels): use default LabelsChart component for labels related widgets

--- src/components/labels/LabelsBubble.js
+++ src/components/labels/LabelsBubble.js
@@ -1,73 +1,34 @@
 import React, { Component } from 'react'
-import LabelsIcon from 'react-icons/lib/fa/tags'
-import { TrapApiError, Widget, WidgetHeader, WidgetBody, WidgetLoader } from '@mozaik/ui'
 import { ResponsiveBubble } from 'nivo'
-import { labelsPropTypes, labelsDefaultProps } from './propTypes'
-import { countLabel } from './counts'
+import LabelsChart from './LabelsChart'
 
-export default class LabelsBubbles extends Component {
-    static propTypes = labelsPropTypes
-
-    static defaultProps = labelsDefaultProps
-
-    static getApiRequest({ project }) {
-        return {
-            id: `gitlab.projectLabels.${project}`,
-            params: { project },
-        }
-    }
+export default class LabelsBubble extends Component {
+    static getApiRequest = LabelsChart.getApiRequest
 
     render() {
-        const { apiData, apiError, title, countBy, animate } = this.props
-
-        let body = <WidgetLoader />
-        let subject = null
-        let count = 0
-        if (apiData) {
-            const { project, labels } = apiData
-
-            count = labels.length
-
-            const data = {
-                name: 'labels',
-                color: '#000',
-                children: labels,
-            }
-
-            subject = (
-                <a href={project.web_url} target="_blank">
-                    {project.name}
-                </a>
-            )
-
-            body = (
-                <ResponsiveBubble
-                    root={data}
-                    labelSkipRadius={12}
-                    value={countBy}
-                    label={d => `${d.name} ${d[countBy]}`}
-                    labelTextColor="inherit:darker(1.6)"
-                    leavesOnly={true}
-                    colorBy={d => d.color}
-                    animate={animate}
-                />
-            )
-        }
-
         return (
-            <Widget>
-                <WidgetHeader
-                    title={title || `Labels by ${countLabel(countBy)}`}
-                    subject={title ? null : subject}
-                    count={count}
-                    icon={LabelsIcon}
-                />
-                <WidgetBody>
-                    <TrapApiError error={apiError}>
-                        {body}
-                    </TrapApiError>
-                </WidgetBody>
-            </Widget>
+            <LabelsChart {...this.props}>
+                {({ labels, countBy, animate }) => {
+                    const data = {
+                        name: 'labels',
+                        color: '#000',
+                        children: labels,
+                    }
+
+                    return (
+                        <ResponsiveBubble
+                            root={data}
+                            labelSkipRadius={12}
+                            value={countBy}
+                            label={d => `${d.name} ${d[countBy]}`}
+                            labelTextColor="inherit:darker(1.6)"
+                            leavesOnly={true}
+                            colorBy={d => d.color}
+                            animate={animate}
+                        />
+                    )
+                }}
+            </LabelsChart>
         )
     }
 }
--- src/components/labels/LabelsPie.js
+++ src/components/labels/LabelsPie.js
@@ -1,70 +1,31 @@
 import React, { Component } from 'react'
-import LabelsIcon from 'react-icons/lib/fa/tags'
-import { TrapApiError, Widget, WidgetHeader, WidgetBody, WidgetLoader } from '@mozaik/ui'
 import { ResponsivePie } from 'nivo'
-import { labelsPropTypes, labelsDefaultProps } from './propTypes'
-import { countLabel } from './counts'
+import LabelsChart from './LabelsChart'
 
 export default class LabelsPie extends Component {
-    static propTypes = labelsPropTypes
-
-    static defaultProps = labelsDefaultProps
-
-    static getApiRequest({ project }) {
-        return {
-            id: `gitlab.projectLabels.${project}`,
-            params: { project },
-        }
-    }
+    static getApiRequest = LabelsChart.getApiRequest
 
     render() {
-        const { apiData, apiError, title, countBy, animate } = this.props
-
-        let body = <WidgetLoader />
-        let subject = null
-        let count = 0
-        if (apiData) {
-            const { project, labels } = apiData
-
-            count = labels.length
-
-            const data = labels.map(label => ({
-                ...label,
-                id: `${label.id}`,
-                value: label[countBy],
-            }))
-
-            subject = (
-                <a href={project.web_url} target="_blank">
-                    {project.name}
-                </a>
-            )
-
-            body = (
-                <ResponsivePie
-                    data={data}
-                    colorBy={d => d.color}
-                    innerRadius={0.6}
-                    label={d => `${d.name} ${d.open_issues_count}`}
-                    animate={animate}
-                />
-            )
-        }
-
         return (
-            <Widget>
-                <WidgetHeader
-                    title={title || `Labels by ${countLabel(countBy)}`}
-                    subject={title ? null : subject}
-                    count={count}
-                    icon={LabelsIcon}
-                />
-                <WidgetBody>
-                    <TrapApiError error={apiError}>
-                        {body}
-                    </TrapApiError>
-                </WidgetBody>
-            </Widget>
+            <LabelsChart {...this.props}>
+                {({ labels, countBy, animate }) => {
+                    const data = labels.map(label => ({
+                        ...label,
+                        id: `${label.id}`,
+                        value: label[countBy],
+                    }))
+
+                    return (
+                        <ResponsivePie
+                            data={data}
+                            colorBy={d => d.color}
+                            innerRadius={0.6}
+                            label={d => `${d.name} ${d[countBy]}`}
+                            animate={animate}
+                        />
+                    )
+                }}
+            </LabelsChart>
         )
     }
 }
